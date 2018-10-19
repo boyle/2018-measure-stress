f2928f0d3492e8902b0b2d56a1e9b44eade6c430
Alistair Boyle <alistair.js.boyle@gmail.com>
2018-10-19 02:03:28 -0400

    www: learnt to respond in JSON when asked

--- www/app.py
+++ www/app.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
-from flask import render_template
+from flask import render_template, jsonify
 from werkzeug import secure_filename
 import re
 
@@ -73,20 +73,26 @@ def listsubdirs(base):
 def listfiles(base):
     return [ o for o in os.listdir(base) ]
 
+def responselist(ret):
+    if request.content_type == 'application/json':
+        return jsonify(ret)
+    else:
+        return '<br/>'.join(ret)
+
 @app.route('/api')
 def apiversionlist():
-    return '<br/>'.join(['v1'])
+    return responselist(['v1'])
 
 @app.route('/api/v1')
 def patientlist():
     base = app.config['UPLOAD_FOLDER']
-    return '<br/>'.join(listsubdirs(base))
+    return responselist(listsubdirs(base))
 
 @app.route('/api/v1/<int:patient>')
 def sessionlist(patient):
     base = app.config['UPLOAD_FOLDER']
     path = os.path.join(base, str(patient))
-    return '<br/>'.join(listsubdirs(path))
+    return responselist(listsubdirs(path))
 
 @app.route('/api/v1/<int:patient>/<int:session>')
 def datalist(patient,session):
@@ -96,7 +102,7 @@ def datalist(patient,session):
                                    str(session)))
     regex = re.compile(r'\.[0-9]+$')
     files = filter(lambda i: not regex.search(i), files)
-    return '<br/>'.join(files)
+    return responselist(files)
 
 @app.route('/api/v1/<int:patient>/<int:session>/<string:measure>')
 def returnfile(patient, session, measure):
