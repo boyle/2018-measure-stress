5088077171e66454778164298a1d729fff1302cf
Francois Charih <francois.charih@gmail.com>
2018-10-25 21:27:18 -0400

    Fix dependencies to avoid bug related to buttons
    - Update dependencies in package.json
    - Clean setup.sh so that the problematic packages are installed as through the "npm install" command

--- app/setup.sh
+++ app/setup.sh
@@ -7,17 +7,14 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 
-# Install the node modules
-npm install --save  react-native@0.57.1 # newest version (0.57.2) causes crashes with Buttons
-npm add @babel/runtime
-npm i schedule@0.4.0 --save-dev
+# Install the node modules (dependencies)
 npm install
 
 # Install the react-native CLI (requires sudo because global)
-#sudo npm install -g react-native-cli
+sudo npm install -g react-native-cli
 
 # Link the react-native-svg library
-sudo react-native link react-native-svg
+react-native link react-native-svg
 
 # Set the path to the SDK
 echo "sdk.dir=$1" > android/local.properties
