35a306fe51909774f51e88f7f5767160bb7314ec
Francois Charih <francois.charih@gmail.com>
2019-03-22 19:58:05 -0400

    Add a common event.

--- app/globals/common_events.js
+++ app/globals/common_events.js
@@ -6,12 +6,12 @@ export default [
   { label: "Harness removal", icon: "tshirt", color: "red" },
   { label: "Marker removal", icon: "map-marker", color: "red" },
   { label: "Patient sitting", icon: "chair", color: "black" },
-  //  {
-  //    label: "Patient leaves treatment area",
-  //    icon: "door-closed",
-  //    color: "black"
-  //  },
-  { label: "Guided relaxation", icon: "yin-yang", color: "black" },
+  {
+    label: "Patient enters treatment area",
+    icon: "door-closed",
+    color: "black"
+  },
   { label: "Repositioning a marker", icon: "map-marker", color: "orange" },
+  { label: "Guided relaxation", icon: "yin-yang", color: "black" },
   { label: "Patient education", icon: "chalkboard-teacher", color: "black" }
 ];
