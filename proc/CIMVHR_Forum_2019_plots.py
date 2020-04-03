e05dd1e513b219bc91fa229b7e237ce67f565272
Alistair Boyle <alistair.js.boyle@gmail.com>
2020-04-03 17:20:37 -0400

    mostly whitespace changes

--- proc/CIMVHR_Forum_2019_plots.py
+++ proc/CIMVHR_Forum_2019_plots.py
@@ -3,7 +3,7 @@
 
 # CIMVHR Forum 2019 Poster Plots
 # -----------------------------------
-# 
+#
 # This notebook was created 2019-09-30 to make plots for the CIMVHR Forum 2019 poster "Machine Learning for the Prediction of Autonomic Nervous System Response during Virtual Reality Treatment using Biometric Data"
 
 # ECG to HRV
@@ -16,7 +16,7 @@ import stress
 import stress.pan_tompkins
 import math
 import numpy as np
-from matplotlib import pyplot as plt 
+from matplotlib import pyplot as plt
 
 fn = 'ECG/2018_12_05-10_35_00_ECG.csv.gz'
 ecg, fs = stress.read_ecg(fn)
@@ -38,7 +38,7 @@ print('%d QRS complexes found'%(len(qrs)))
 # In[55]:
 
 
-plt.clf() 
+plt.clf()
 ax = plt.subplot(311)
 stress.plot_ecg(ecg, qrs, offset=offset, window=window)
 #plt.title(fn)
@@ -70,47 +70,47 @@ stress.plot_hr_sqi(t_qrs, sqi, offset=offset, window=window)
 
 
 fig = plt.gcf()
-fig.set_size_inches(9, 8)
+fig.set_size_inches(18, 8)
 plt.savefig('ecg.pdf', transparent=True,
             bbox_inches='tight', pad_inches=0)
 plt.show()
 
 
 # The original Pan-Tompkins algorithm is
-# 
+#
 # >  [1] Jiapu Pan, Willis J. Tompkins. "A real-time QRS detection algorithm."
-# >  IEEE Transactions onBiomedical Engineering 3 (1985): 230-236.    
-# 
+# >  IEEE Transactions onBiomedical Engineering 3 (1985): 230-236.
+#
 # There are many implementations. The filtering process is simple, but the devil is in the details: the adaptive thresholds, where each implementation makes different choices. For many data sets where the ECG is relatively clean, these choices don't seem to make much difference, but for our data many implementations produce quite poor results. We base our implementation off an [MIT licensed](https://opensource.org/licenses/MIT) (open source, no restrictions) python code from [github.com/pickus91/HRV](https://github.com/pickus91/HRV).
-# 
+#
 # The original Pan-Tompkins QRS detector (including this python implementation) find very few beats and most of those are noise.
 # Initialization fails because the start of the recording is noisy
 # (lots of movement as the instrument is turned on, attached to the patient,
 #  and the patient moves into the virtual reality simulator).
 # Even with a good initialization, noise due to movement will often drive the Pan-Tompkins
 # thresholds into a region where it will never detect another QRS complex.
-# 
+#
 # Note the option `decay = None`, disabling new code.
 
 # By allowing the thresholds to decay after 10x the expected max R-R interval,
 # at a rate set by `decay` per detected signal peak,
 # we can now recover when these detection thresholds are either initialized
 # to a large value or are driven to a large value due to random noise (for example, movement artifacts).
-# 
+#
 # Note the option `decay != None`.
-# 
+#
 # We now detect many beats for most available ECG recordings and the R peaks are quite accurate, even for moderately noisy data.
 
 # Using the R peaks detected by the modified Pan-Tompkins, we can calculate R-R intervals (heart rate, HR)
 # in beats per minute (bpm), and the heart rate variability (HRV) in beats per minute.
-# 
+#
 # For HRV, there are many alternatives to Root Mean Square of Successive Differences (RMSSD). See [wikipedia/Heart_rate_variability](https://en.wikipedia.org/wiki/Heart_rate_variability) for some examples.
 
 # And plot signal quality index (SQI) from
-# 
+#
 # >  [2] M. Abdelazez, P. Quesnel, A. D. C. Chan, H. Yang. Signal Quality Analysis of Ambulatory Electrocardiograms to Gate False Myocardial Ischemia Alarms.
 # > IEEE Transactions on Biomedical Engineering 64(6) 1318-1325, 2017. [10.1109/TBME.2016.2602283](https://doi.org/10.1109/TBME.2016.2602283)
-# 
+#
 
 # In[26]:
 
@@ -159,7 +159,7 @@ ax.grid(False)
 plt.title('Marker Motion Capture', y=+0.85)
 
 fig = plt.gcf()
-fig.set_size_inches(9,8)
+fig.set_size_inches(8,8)
 plt.savefig('c3d.pdf', transparent=True,
             bbox_inches='tight', pad_inches=0)
 plt.show()
@@ -177,13 +177,13 @@ ax.set_title('Centre of Pressure')
 ax.set_xlabel('x (mm)')
 ax.set_ylabel('y (mm)')
 
-      
+
 ax = plt.subplot2grid((2, 1), (1, 0), colspan=3)
 stress.plot_force_plates(force, frame_num=frame_num, fs=fs)
 ax.legend(loc='upper left', ncol=3, frameon=False)
-        
+
 fig = plt.gcf()
-fig.set_size_inches(9, 8)
+fig.set_size_inches(18, 8)
 plt.savefig('force.pdf', transparent=True,
             bbox_inches='tight', pad_inches=0)
 plt.show()
